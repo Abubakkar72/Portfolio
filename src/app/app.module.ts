ef3b392f9c6246ff11c2772665e1a91cbc47cfae
Abubakkar72 <abubakkarsiddiqn697@gmail.com>
2024-02-04 19:54:25 +0530

    work done

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './views/home/home.module';
+import { AboutComponent } from './views/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
