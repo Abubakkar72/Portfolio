36048f55474c9c09babdfad5cd01d5930ba1fc3e
Abubakkar72 <abubakkarsiddiqn697@gmail.com>
2024-02-07 16:48:06 +0530

    updated work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: DashboardComponent},
+  {path: 'project', component: ProjectsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './views/home/home.module';
+import { HomeModule } from './views/home.module';
 import { AboutComponent } from './views/about/about.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
